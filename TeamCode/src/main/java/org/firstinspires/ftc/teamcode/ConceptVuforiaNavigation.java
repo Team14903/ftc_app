145a0a90a8679772c94d2b77849a20bac723bf89
MSI\ayush <ayu02468@gmail.com>
2018-10-02 21:15:40 -0400

    Turning Vuforia code into an Autonomous program

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConceptVuforiaNavigation.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConceptVuforiaNavigation.java
@@ -29,6 +29,7 @@
 
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -80,7 +81,7 @@ import java.util.List;
  * is explained below.
  */
 
-@TeleOp(name="Concept: Vuforia Navigation", group ="Concept")
+@Autonomous(name="Concept: Vuforia Navigation", group ="Concept")
 //@Disabled
 public class ConceptVuforiaNavigation extends LinearOpMode {
 
