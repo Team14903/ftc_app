b482d6e43eba22a248cc45ab3c49d0c24d12f11f
MSI\ayush <ayu02468@gmail.com>
2018-11-24 16:37:56 -0500

    Added a commented out @Disabled

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoverRuckusTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoverRuckusTeleOp.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -7,7 +8,7 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.TouchSensor;
-
+//@Disabled
 @TeleOp(name= "TeleOp Basic Program")
 public class RoverRuckusTeleOp extends LinearOpMode {
 
